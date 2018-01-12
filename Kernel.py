2d38728fb6a2984a5691d81d9871a71d1e57817f
buttlet <atlisigurgeirsson@gmail.com>
2018-01-12 15:35:49 +0100

    modified Kernel.py to be more class-based

--- Kernel.py
+++ Kernel.py
@@ -1,56 +1,58 @@
 from nltk.corpus import reuters, stopwords
 import operator
+from random import randint 
 
-def main():
-    # List of documents
-    print(stopwords.words('english'))
-    documents = reuters.fileids()
-    print(str(len(documents)) + " documents")
+'''
+train_docs = list(filter(lambda doc: doc.startswith("train"), reuters.fileids()))
+test_docs = list(filter(lambda doc: doc.startswith("test"), reuters.fileids()))
+categories = reuters.categories()
+'''
 
-    train_docs = list(filter(lambda doc: doc.startswith("train"), documents))
-    print(str(len(train_docs)) + " total train documents")
+class Kernel:
+    '''A SSK kernel class'''
+    def __init__(self, n, document):
+        self.n = n
+        self.document = document
+        self.document.set_features(n)
+        self.document.set_freq_features()
+        print(self.document.freq_features)
+    
+    def __repr__(self):
+        print('A kernel with n: '+ str(self.n))
 
-    test_docs = list(filter(lambda doc: doc.startswith("test"),
-                            documents))
-    print(str(len(test_docs)) + " total test documents")
+class Document:
+    '''A class for a document from the Reuters data-set'''
+    def __init__(self, category, index):
+        self.category = category
+        self.index = index
+        self.id = reuters.fileids(category)[index]
+        self.words = reuters.words(self.id)
+        self.clean_data = self.remove_stops()
+    
+    def get_words(self):
+        return reuters.words
+    
+    def remove_stops(self):
+        special_char = [".", ",", ":", "", "_", "-", "&", "%", "<", ">", "!", "?", "="]
+        cleaned = [x.lower() for x in self.words if x not in special_char and x.lower() not in stopwords.words('english')]
+        return ' '.join(cleaned)
+    
+    def set_features(self, n=4):
+        '''returns the complete list of contigous letter combinations of length n'''
+        self.features = set()
+        for i in range(len(self.clean_data)-n+1):
+            self.features.add(self.clean_data[i:i+n])
 
-    # List of categories
-    categories = reuters.categories()
-    print(str(len(categories)) + " categories")
+    def set_freq_features(self):
+        '''returns features in the order of number of occurrences'''
+        tuples = {}
+        for f in self.features:
+            tuples[f] = self.clean_data.count(f)
+        tuples_sorted = sorted(tuples, key=tuples.get, reverse=True)
+        self.freq_features = tuples_sorted
 
-    # Documents in a category
-    category_docs = reuters.fileids("earn")
-
-    # Words for a document
-    document_id = category_docs[0]
-    document_words = reuters.words(category_docs[0])
-
-    doc = removeStopwords(reuters.words(category_docs[0]))
-    features = featureExtraction(doc,4)
-    f = freqFeatures(features,doc)
-    print(f)
-
-    # Raw document
-    print(reuters.raw(document_id))
-
-def removeStopwords(document):
-    specialCharacters = [".", ",", ":", "", "_", "-", "&", "%", "<", ">", "!", "?", "="]
-    cleaned = [x.lower() for x in document if x not in specialCharacters and x.lower() not in stopwords.words('english')]
-    return ' '.join(cleaned)
-
-def featureExtraction(document, k):
-    features = set()
-    for i in range(len(document)-k+1):
-        features.add(document[i:i+k])
-    return features
-
-def freqFeatures(features, document):
-    tuples = {}
-    for f in features:
-        tuples[f] = document.count(f)
-    tuples_sorted = sorted(tuples, key=tuples.get, reverse=True)
-    return tuples_sorted
-
-main()
-
-
+    def __repr__(self):
+        return 'category: ' + self.category + '\n'\
+        + 'index: ' + str(self.index) + '\n'\
+        +'----------------------------\n RAW DOCUMENT:'\
+        +reuters.raw(self.id)
