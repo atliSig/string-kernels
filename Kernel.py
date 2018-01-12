1905811acb144538d0a0c2f7879774eb8b4fef50
buttlet <atlisigurgeirsson@gmail.com>
2018-01-12 12:26:23 +0100

    added to readme and more

--- Kernel.py
+++ Kernel.py
@@ -5,36 +5,36 @@ def main():
     # List of documents
     print(stopwords.words('english'))
     documents = reuters.fileids()
-    print(str(len(documents)) + " documents");
+    print(str(len(documents)) + " documents")
 
-    train_docs = list(filter(lambda doc: doc.startswith("train"), documents));
-    print(str(len(train_docs)) + " total train documents");
+    train_docs = list(filter(lambda doc: doc.startswith("train"), documents))
+    print(str(len(train_docs)) + " total train documents")
 
     test_docs = list(filter(lambda doc: doc.startswith("test"),
-                            documents));
-    print(str(len(test_docs)) + " total test documents");
+                            documents))
+    print(str(len(test_docs)) + " total test documents")
 
     # List of categories
-    categories = reuters.categories();
-    print(str(len(categories)) + " categories");
+    categories = reuters.categories()
+    print(str(len(categories)) + " categories")
 
     # Documents in a category
-    category_docs = reuters.fileids("earn");
+    category_docs = reuters.fileids("earn")
 
     # Words for a document
     document_id = category_docs[0]
-    document_words = reuters.words(category_docs[0]);
+    document_words = reuters.words(category_docs[0])
 
     doc = removeStopwords(reuters.words(category_docs[0]))
     features = featureExtraction(doc,4)
     f = freqFeatures(features,doc)
-    print(f);
+    print(f)
 
     # Raw document
-    print(reuters.raw(document_id));
+    print(reuters.raw(document_id))
 
 def removeStopwords(document):
-    specialCharacters = [".", ",", ":", ";", "_", "-", "&", "%", "<", ">", "!", "?", "="]
+    specialCharacters = [".", ",", ":", "", "_", "-", "&", "%", "<", ">", "!", "?", "="]
     cleaned = [x.lower() for x in document if x not in specialCharacters and x.lower() not in stopwords.words('english')]
     return ' '.join(cleaned)
 
--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 1. Install requirements via `pip install -r requirements.txt`
 2. For tokenization, install `punkt` via  `nltk.download('punkt')`
 3. For tree parsing, install `treebank` via  `nltk.download('treebank')`
+4. For stop words, install `stopwords` via  `nltk.download('stopwords')`
+5. For the Reuters data-set, install `reuters` via  `nltk.download('reuters')`
